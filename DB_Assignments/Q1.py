42938a1a48ef40d423dd4c188f049fdf4bc8f524
Coder-12 <31615917+Coder-12@users.noreply.github.com>
2020-12-12 09:44:08 +0530

    Add files via upload

---
+++ DB_Assignments/Q1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import pyspark
+from pyspark.sql import *
+import argparse
+
+parser = argparse.ArgumentParser(description='Solver')
+parser.add_argument('outpath',type=str,metavar='O',help='path to output')
+parser.add_argument('--ncpu',  type=int, default=2,help='number of cpus')
+
+args = parser.parse_args()
+
+spark = SparkSession.builder \
+  .master("local") \
+  .appName("Word Count") \
+  .config("spark.some.config.option", "some-value") \
+  .getOrCreate()
+
+airports = spark.read.csv("Dataset/airports.csv",
+        header=True,
+        inferSchema=True)
+
+df = airports.groupby("COUNTRY").count()
+df.toPandas().to_csv(args.outpath, index=False)
